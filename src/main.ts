7d62e3efe00c11ecbbd33835ecf68d07f29c670b
hz <15738818183@163.com>
2020-08-12 16:34:45 +0800

    临时提交

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ const { version } = require('../package.json')
 let actionMap = {
   description: 'create a new project from a template',
   usages: [
-    'eyes create [projectName]',
-    'eyes --help',
-    'eyes -V'
+    'eyes create [projectName]   创建项目',
+    'eyes -V, --version          版本号',
+    'eyes -h, --help             帮助'
   ],
   alias: 'i'
 }
@@ -17,27 +17,24 @@ commander
   .command('eyes')
   .usage('create <fileName>')
   .version(version)
-  // .option('-r, --range <a>..<b>', 'A range', range)
+  .option('-h, --help', '帮助', handleHelp)
   // .option('-l, --list <items>', 'A list', list)
-  .action((d: any, otherD: any) => {
-    const cmdStr = otherD ? otherD[0] : ''
+  .action((d: any, argvs: any) => {
+    const cmdStr = argvs ? argvs[0] : ''
     switch (cmdStr) {
       case 'create':
-        if (!otherD[1]) {
+        if (!argvs[1]) {
           throw new Error('请输入文件名')
         }
-        apply('create', otherD[1])
-        break
-      default:
-        handleHelp()
+        apply('create', argvs[1])
         break
   })
   .parse(process.argv)
 
-  function handleHelp() {
-    console.log(chalk.blue('\r\nUsage:'))
-    actionMap.usages.forEach(item => {
-      console.log(chalk.blue('  - ' + item))
-    })
-    console.log('\r')
-  }
+function handleHelp() {
+  console.log(chalk.blue('\r\nUsage:'))
+  actionMap.usages.forEach(item => {
+    console.log(chalk.blue('  - ' + item))
+  })
+  console.log('\r')
+}
